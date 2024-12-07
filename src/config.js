8d04178c89dc5ffd8d69450e20fbbea2938e7969
avmaurya07 <avmaurya07@gmail.com>
2024-12-07 11:16:03 +0530

    update

--- src/config.js
+++ src/config.js
@@ -3,9 +3,8 @@ const config =  {
     // host : "https://f6672jpz-5000.inc1.devtunnels.ms",
     // host : "http://localhost:5000",
     // host : "https://erpsystemserver.avmaurya07.tech", 
-    MONGODB_URI :"mongodb+srv://avmaurya07:avmaurya07@cluster0.nxhd2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-baseURL : "http://ec2-43-204-148-21.ap-south-1.compute.amazonaws.com:1337/api/",
-// baseURL : "https://5130-103-47-74-130.ngrok-free.app/api/",
+//    baseURL : "http://ec2-43-204-148-21.ap-south-1.compute.amazonaws.com:1337/api/",
+baseURL : "https://mak-doctor-plus.vercel.app/api/",
 EXPO_PUBLIC_STRAPI_API_KEY : "503ca2c0c52c0a0065fd503b75b09c6a1f5215ea8c3b5209a2b9b4e58fe3973d68dbcca7b528a541fbb72c8d4f76720475e00b84aa00260beea71993eebf28e3f5baed9d27514a1ee9a65448757cfcc0bbada39780e52727f4dd49b1072d604523aa7dda8533b96f52f02d6c5c92b0d412e31e01141bec80e21eda85d6539885"
 
 }
